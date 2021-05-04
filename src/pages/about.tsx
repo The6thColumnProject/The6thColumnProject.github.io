45b4ebad639c81f814fa9cb5b5998363687c8b82
Max G. Faraday <maxgfaraday@6thcolumn.org>
2021-05-04 20:59:36 +0200

    Updated About page (edit 2)

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -16,37 +16,62 @@ const AboutPage = () => {
       <Segment vertical>
         <p>
 
-        The philosophy of the 6th Column Project is that we can do
-        more to create a seamless interaction between people and our
-        computing environment.  We also believe that computing as it
-        is cast is due for an evolutionaly step change.  W3 and
-        peer-to-peer efforts are necessary for us truly bring forth an
-        era where compting even more greatly reduces toil and becomes
-        an integral part in cooperative problem solving. Aggressive
-        automation to rid ourselves from incidental complexity just
-        from combersome interfaces and less than streamlined processes
-        and work flows.
+          The philosophy of the 6th Column Project is that we can do
+          more to create a seamless interaction between people and our
+          computing environment.  We also believe that computing as is
+          due for an evolutionary step change.  W3 and peer-to-peer
+          efforts are necessary tenets for us truly bring forth an era
+          where computing reduces toil even more profoundly and becomes
+          an integral part in cooperative problem solving. Aggressive
+          automation will greatly reduce the pain from incidental
+          complexity, cumbersome interfaces and convoluted processes.
+
         </p>
 
         <p>
-        The team has consistently solved big problems in various areas
-        of computing.  From the financial sector (Wall St & major
-        FinTechs), to pure science research in national science
-        laboratories, to high throughput streaming, to creating and
-        building Nobel Prize winning climate science peer-to-peer
-        infrastructure - the team here is of seasoned problem solvers.
+
+          Our team has consistently solved big problems in various areas of computing at scale.
+          <ul>
+            <li><b>Financial sector:</b> Building trading and portfolio
+              management systems for Wall Street.  Creating infrastructure
+              supporting major FinTechs.</li>
+
+          <li><b>Pure science research:</b> Working in the worldwide national
+            laboratory system solving problems in energy and climate
+            science.  Building critical aspects of advanced research
+            energy fusion systems to architecting and building a
+            world-wide peer-to-peer system for the transport and
+            sharing of climate data, and computation of climate
+            science models around the world. (Recipient of a Nobel
+            Prize and R&D 100 award)</li>
+
+          <li><b>Platforms:</b> High throughput, durable media
+            streaming platforms, supporting millions of simultaneous
+            clients around the world.
+
+            Building AI-Ops systems and scaling them to web-scale, for
+            the largest cloud provider in the world. Customer_Company
+            names...</li>
+
+          </ul>
         </p>
-        
+
+        <p>
+          We are seasoned leaders, problem solvers, pragmatists and futurists.
+        </p>
+
         <p>
 
-        The team has shared singular vision of computing.  The time is
-        right. The confluence of technology and the raised awareness
-        and sophistication of the general populous around technology
-        has created an appetite for releaving our current fatigue with
-        the status quo. We are ready to engage in a new way.
+        The team has a shared vision of computing.  The confluence of
+        technologies and the raised awareness and technical
+        sophistication of the general populous has created the right
+        conditions to introduce compelling solutions.  The 6th Column
+        Project is crafting solutions that will relieve our current
+        fatigue with the status quo and empower us to solve problems
+        cooperatively.
 
         </p>
-        
+
         <p>
         <b>Stay Tuned...</b>
         </p>
